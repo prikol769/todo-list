3da2c0eeff7d06515b9d7e32952e730e47f1d66f
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-01 16:20:14 -0500

    added app component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ListItems from "./components/ListItems";
+
+function App() {
+  return (
+    <>
+      <h1>Todo List</h1>
+      <ListItems />
+    </>
+  );
+}
+
+export default App;
