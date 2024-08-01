ba03716f83525d407dcd7044137437d101d9b1ed
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-01 16:20:53 -0500

    added ListItem component

---
+++ src/components/ListItems.jsx
@@ -0,0 +1,61 @@
+import { useState, useReducer } from "react";
+import ListItem from "./ListItem";
+import { initialState } from "../data/data";
+import { itemReducer } from "../reducers/ItemReducer";
+
+const ListItems = () => {
+  const [items, dispatch] = useReducer(itemReducer, initialState);
+  const [newItemText, setNewItemText] = useState("");
+
+  const addItemHandler = () => {
+    dispatch({
+      type: "addItem",
+      payload: { userId: 1, title: newItemText, completed: false },
+    });
+    setNewItemText("");
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: 15,
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          value={newItemText}
+          onChange={(e) => setNewItemText(e.target.value)}
+          placeholder="Add task"
+        />
+        <button onClick={addItemHandler}>Add</button>
+      </div>
+      {items.length === 0 ? (
+        <p>There are no items in the list.</p>
+      ) : (
+        items.map((item) => (
+          <ListItem
+            key={item.id}
+            item={item}
+            handleCheck={(id) =>
+              dispatch({ type: "toggleItem", payload: { id } })
+            }
+            handleDelete={(id) =>
+              dispatch({ type: "deleteItem", payload: { id } })
+            }
+            handleSave={(id, title) =>
+              dispatch({ type: "saveItem", payload: { id, title } })
+            }
+          />
+        ))
+      )}
+      {}
+    </div>
+  );
+};
+
+export default ListItems;
